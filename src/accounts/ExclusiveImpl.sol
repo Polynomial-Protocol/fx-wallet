0307d1d7f7ac3b6a253fa68383f2d6445e322575
ankitpal1029 <=>
2023-08-03 15:46:54 +0530

    💉 extract function selector from bytes memory

--- src/accounts/ExclusiveImpl.sol
+++ src/accounts/ExclusiveImpl.sol
@@ -2,26 +2,29 @@
 pragma solidity ^0.8.9;
 
 import {Variables} from "./Variables.sol";
+import {Controllers} from "../registry/Connectors.sol";
 
 interface ConnectorsInterface {
     function isConnectors(string[] calldata connectorNames) external view returns (bool, address[] memory);
 }
 
-contract Constants is Variables {
+contract Constants is Variables, Controllers {
     // polyIndex Address.
     address internal immutable polyIndex;
     // Connectors Address.
     address public immutable connectors;
     // Additional Auth Module(Address of Additional Auth => bool).
     mapping(address => bool) internal _additionalAuth;
+    // Restricted targets and function selector
+    mapping(string => mapping(bytes4 => bool)) internal _restrictedTargetsAndSelectors;
 
-    constructor(address _polyIndex, address _connectors) {
+    constructor(address _polyIndex, address _connectors) Controllers(_polyIndex) {
         connectors = _connectors;
         polyIndex = _polyIndex;
     }
 }
 
-contract ExclusiveImplementation is Constants {
+contract ExclusiveImplementation is Constants{
     constructor(address _polyIndex, address _connectors) Constants(_polyIndex, _connectors) {}
     
     event Exclusive(uint256 data, address where);
@@ -45,15 +48,28 @@ contract ExclusiveImplementation is Constants {
     event LogDisableAdditionalUser (
         address indexed user
     );
-        
-
+    
+    
     /**
-     * @dev Check for Auth if enabled.
-     * @param user address/user/owner.
+     * @dev Setting target and calldata that are restricted
+     * @param _target Target in which the function exists
+     * @param _functionSelector Function selector restricted from being called by exclusiveCast
      */
-    function isAuth(address user) public view returns (bool) {
-        return _auth[user];
+    function setTargetAndCallData(string memory _target, bytes4 _functionSelector) external isChief returns (bool) {
+        _restrictedTargetsAndSelectors[_target][_functionSelector] = true;
+        return true;
     }
+    
+    /**
+     * @dev Removing target and calldata that are restricted
+     * @param _target Target in which the function exists
+     * @param _functionSelector Function selector restricted from being called by exclusiveCast
+     */
+     function removeTargetAndCallData(string memory _target, bytes4 _functionSelector) external isChief returns (bool) {
+            delete _restrictedTargetsAndSelectors[_target][_functionSelector];
+            return true; 
+     }
+
     
     /**
      * @dev Check if Beta mode is enabled or not
@@ -76,8 +92,8 @@ contract ExclusiveImplementation is Constants {
      * @dev Enable New User.
      * @param user Owner address
      */
-    function enable(address user) public {
-        require(msg.sender == address(this) || msg.sender == polyIndex || isAuth(msg.sender), "not-self-index");
+    function enable(address user) public isAuth(msg.sender){
+        require(msg.sender == address(this) || msg.sender == polyIndex , "not-self-index");
         require(user != address(0), "not-valid");
         require(!_additionalAuth[user], "already-enabled");
         _additionalAuth[user] = true;
@@ -88,8 +104,8 @@ contract ExclusiveImplementation is Constants {
      * @dev Disable User.
      * @param user Owner address
      */
-    function disable(address user) public {
-        require(msg.sender == address(this) || msg.sender == polyIndex || isAuth(msg.sender), "not-self");
+    function disable(address user) public isAuth(msg.sender){
+        require(msg.sender == address(this) || msg.sender == polyIndex, "not-self");
         require(user != address(0), "not-valid");
         require(_auth[user], "already-disabled");
         delete _auth[user];
@@ -162,6 +178,51 @@ contract ExclusiveImplementation is Constants {
 
         return ecrecover(_message, v, r, s);
     }
+    
+    function getFunctionSelectorBytesMemory(
+        bytes memory _bytes
+    )
+        internal
+        pure
+        returns (bytes4)
+    {
+        uint256 _start = 0;
+        uint256 _length = 4;
+        require(_length + 31 >= _length, "slice_overflow");
+        require(_bytes.length >= _start + _length, "slice_outOfBounds");
+
+        bytes memory tempBytes;
+
+        assembly {
+            switch iszero(_length)
+            case 0 {
+                tempBytes := mload(0x40)
+
+                let lengthmod := and(_length, 32)
+
+                let mc := add(add(tempBytes, lengthmod), mul(0x20, iszero(lengthmod)))
+                let end := add(mc, _length)
+
+                for {
+                    let cc := add(add(add(_bytes, lengthmod), mul(0x20, iszero(lengthmod))), _start)
+                } lt(mc, end) {
+                    mc := add(mc, 0x20)
+                    cc := add(cc, 0x20)
+                } {
+                    mstore(mc, mload(cc))
+                }
+                mstore(tempBytes, _length)
+                mstore(0x40, and(add(mc, 31), not(31)))
+            }
+            default {
+                tempBytes := mload(0x40)
+                mstore(tempBytes, 0)
+                mstore(0x40, add(tempBytes, 0x20))
+            }
+        }
+
+        return bytes4(tempBytes);
+    }
 
 
     /**
@@ -183,22 +244,23 @@ contract ExclusiveImplementation is Constants {
     
         address signerOfMessage = ecrecover(hashedTargetNamesAndCallData, v, r, s);
         
-        /*
-            Step 1: check if signer of message is in additional auth or not - done
-            Step 2: check if it's been 15 mins since the timestamp if so revert - done
-            Step 3: check if certain targets and calldata are being used which shouldn't (need to make seperate module for this)
-        */
         require(_additionalAuth[signerOfMessage], "not-authorized");
         require(isBeta(), "beta-not-enabled");
         
         
         (string[] memory _targetNames, bytes[] memory _datas, uint256 timestamp) = abi.decode(_targetNamesAndCallData, (string[], bytes[], uint256));
         require(timestamp + 15 minutes > block.timestamp, "timestamp-invalid");
+        
         uint256 _length = _targetNames.length;
         require(_length != 0, "1: length-invalid");
         require(_length == _datas.length, "1: array-length-invalid");
-        
-        
+
+        for(uint256 i = 0; i < _length; i++) {
+            bytes memory _calldata = _datas[i];
+            bytes4 selector;
+            require(!_restrictedTargetsAndSelectors[_targetNames[i]][getFunctionSelectorBytesMemory(_datas[i])], "restricted-target");
+        }
+
         string[] memory eventNames = new string[](_length);
         bytes[] memory eventParams = new bytes[](_length);
 
@@ -213,6 +275,11 @@ contract ExclusiveImplementation is Constants {
 
         emit LogExclusiveCast(_origin, msg.sender, msg.value, _targetNames, _targets, eventNames, eventParams);
 
+    }
+    
+    modifier isAuth(address user) {
+        require(_auth[user], "not-wallet-owner");
+        _;
     }
     
 }
